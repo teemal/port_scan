8171e13f25c62e603d690b2c3e1af331485d4433
teemal <taymal1987@gmail.com>
2020-02-18 21:23:23 -0800

    finished

--- ps.py
+++ ps.py
@@ -1,12 +1,11 @@
 import dpkt, socket, sys
 
-SYN_SYNACK_RATIO = 3
+THRESHOLD = 3
 
 #=FUNCTIONS===================================================================#
 
 
 def tcpFlags(tcp):
-    """Returns a list of the set flags in this TCP packet."""
     ret = list()
 
     if tcp.flags & dpkt.tcp.TH_FIN != 0:
@@ -30,40 +29,28 @@ def tcpFlags(tcp):
 
 
 def compare_IPs(ip1, ip2):
-    """
-    Return negative if ip1 < ip2, 0 if they are equal, positive if ip1 > ip2.
-    """
     return sum(map(int, ip1.split('.'))) - sum(map(int, ip2.split('.')))
-
-# get local ip address
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-localIP = s.getsockname()[0]
-s.close()
 
 f = open('example.pcap')
 pcap = dpkt.pcap.Reader(f)
 print(pcap)
 
-suspects = dict() # Dictionary of suspects. suspect's IP: {# SYNs, # SYN-ACKs}
-curPacket = 0     # Current packet number.
+# suspect's IP: {# SYNs, # SYN-ACKs}
+suspects = dict()
+curPacket = 0
 
 # Analyze captured packets.
 for ts, buf in pcap:
     curPacket += 1
 
-    # Ignore malformed packets
-    try:
-        eth = dpkt.ethernet.Ethernet(buf)
-    except (dpkt.dpkt.UnpackError, IndexError):
-        continue
+    eth = dpkt.ethernet.Ethernet(buf)
 
-    # Packet must include IP protocol to get TCP
+    # must be IP packets
     ip = eth.data
     if not ip:
         continue
 
-    # Skip packets that are not TCP
+    # skip non TCP
     tcp = ip.data
     if type(tcp) != dpkt.tcp.TCP:
         continue
@@ -74,42 +61,22 @@ for ts, buf in pcap:
     srcIP = socket.inet_ntoa(ip.src)
     dstIP = socket.inet_ntoa(ip.dst)
 
-    # Fingerprint possible suspects.
-    if {'SYN'} == set(tcpFlag):          # A 'SYN' request.
+    # add suspicious IP to list
+    if {'SYN'} == set(tcpFlag):
         if srcIP not in suspects: suspects[srcIP] = {'SYN': 0, 'SYN-ACK': 0}
         suspects[srcIP]['SYN'] += 1
-    elif {'SYN', 'ACK'} == set(tcpFlag): # A 'SYN-ACK' reply.
+    elif {'SYN', 'ACK'} == set(tcpFlag):
         if dstIP not in suspects: suspects[dstIP] = {'SYN': 0, 'SYN-ACK': 0}
         suspects[dstIP]['SYN-ACK'] += 1
 
-# Prune unlikely suspects based on ratio of SYNs to SYN-ACKs.
+# remove suspected IP that isn't 3 times the syn/syn ack
 for s in suspects.keys():
-    if suspects[s]['SYN'] < (suspects[s]['SYN-ACK'] * SYN_SYNACK_RATIO):
+    if suspects[s]['SYN'] < (suspects[s]['SYN-ACK'] * THRESHOLD):
         del suspects[s]
 
-# Output results.
-print "Analyzed", curPacket, "packets:"
-
-if not suspects:
-    print 'no suspicious packets detected...'
 
 for s in sorted(suspects.keys(), cmp=compare_IPs):
     syns = suspects[s]['SYN']
     synacks = suspects[s]['SYN-ACK']
 
     print "{0:15} had {1} SYNs and {2} SYN-ACKs".format(s, syns, synacks)
-
-# srcIP=[]
-
-# for pkt in packets:
-#     if IP in pkt:
-#         srcIP.append(pkt[IP].src + " > " + pkt[IP].dst)
-
-# cnt = Counter()
-# for ip in srcIP:
-#     cnt[ip] += 1
-
-# table = PrettyTable(["IP", "Count"])
-# for ip, count in cnt.most_common():
-#     table.add_row([ip, count])
-# print(table)
